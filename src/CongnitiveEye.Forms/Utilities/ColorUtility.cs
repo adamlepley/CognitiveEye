1582f5730a852a2bc988b33e48d707aba951fe09
Adam Lepley <Alepley@wheels.com>
2018-04-26 11:36:58 -0500

    Added Color Utility and More General Styling

---
+++ src/CongnitiveEye.Forms/Utilities/ColorUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CongnitiveEye.Forms.Utilities
+{
+    public static class ColorUtil
+    {
+        public static Color PrimaryColor = Color.FromHex("#346C81");
+        public static Color SecondaryColor = Color.FromHex("#DADFE1");
+        public static Color AlertColor = Color.FromHex("#F57469");
+    }
+       
+}
---
+++ src/CongnitiveEye.Forms/Views/ProjectMenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CongnitiveEye.Forms.Views
+{
+    public partial class ProjectMenuView : ContentPage
+    {
+        public ProjectMenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
