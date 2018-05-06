ab1e1ac2a1809155807dc4a971bb754921cb2923
Adam Lepley <Alepley@wheels.com>
2018-05-06 18:13:58 -0500

    Added App Center Support

---
+++ src/CongnitiveEye.Forms/Utilities/SecretsUtility.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CongnitiveEye.Forms.Utilities
+{
+    public static class SecretsUtility
+    {
+        public const string AppCenterSecret = "APP_CENTER_SECRET";
+    }
+}
